2480c8f8640e5f050716947edf1f78c06d1d3f21
hollyfromthenexus <holly@fromthe.nexus>
2026-08-09 22:35:00 -0500

    Slight boilerplate polish.

--- DoveDraftPlugin.cs
+++ DoveDraftPlugin.cs
@@ -3,14 +3,19 @@ using Godot;
 
 namespace DoveDraft;
 
+/// <summary>
+/// The root plugin script for DoveDraft. See https://docs.godotengine.org/en/stable/tutorials/plugins/editor/making_plugins.html .
+/// </summary>
 [Tool]
 public partial class DoveDraftPlugin : EditorPlugin
 {
+    /// <inheritdoc/>
     public override void _EnterTree()
     {
         // Initialization of the plugin goes here.
     }
 
+    /// <inheritdoc/>
     public override void _ExitTree()
     {
         // Clean-up of the plugin goes here.
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # DoveDraft
+
+A Godot framework for freaks and nerds.
+
+---
+
+## Requirements
+
+- Godot 4.7
+- YarnSpinner-Godot 0.3.12
+
+## Install
+
+```
+# (in the root of your Godot project)
+cd addons
+git submodule add https://github.com/hollyfromthenexus/DoveDraft.git
+```
+
+...then open the project, build, and enable the plugin from the project settings.
